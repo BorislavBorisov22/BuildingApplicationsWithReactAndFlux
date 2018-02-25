8180c555867927a11aa45b18576d95cd5967d97b
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-02-26 00:56:14 +0200

    Added about component and basic self made routing

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,29 @@
+const React = require('react');
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This application uses the following technologies
+                    <ul>
+                        <li>React</li>
+                        <li>React Router</li>
+                        <li>Flux</li>
+                        <li>Node</li>
+                        <li>Gulp</li>
+                        <li>Browserify</li>
+                        <li>Bootsrap</li>
+                    </ul>
+                </p>
+            </div>
+        );
+    }
+}
+
+module.exports = About;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
 const $ = jQuery = require('jquery');
 const React = require('react');
 
-const Home = require('./components/homePage.js');
+const Home = require('./components/homePage');
+const About = require('./components/about/aboutPage');
 
-React.render(<Home />, document.getElementById('app'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        let ChildComponent;
+
+        switch(this.props.route) {
+            case 'about': ChildComponent = About; break;
+            default: ChildComponent = Home; break;
+        }
+
+        return (
+            <div>
+                <ChildComponent />
+            </div>
+        );
+    }
+}
+
+const render = () => {
+    const route = window.location.hash.substring(1);
+    React.render(<App route={route}/>, document.getElementById('app'));
+};
+
+window.addEventListener('hashchange', render);
+render();
