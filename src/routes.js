51a75fd6160bdb68cbdaf24f8e74a072936e3767
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-02-27 23:40:01 +0200

    Added notFoundPage component

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@
 const React = require('react');
 const Router = require('react-router');
 
-const { DefaultRoute, Route } = Router;
+const { DefaultRoute, Route, NotFoundRoute } = Router;
 
 const routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/homePage')}/>
         <Route name="authors" handler={require('./components/authors/authorPage')}/>
         <Route name="about" handler={require('./components/about/aboutPage')}/>
+        <NotFoundRoute handler={require('./components/common/notFoundPage')}/>
     </Route>
 );
 
