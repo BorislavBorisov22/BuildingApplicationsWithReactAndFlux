e2b20d4e6677e3c4209a57e030a6c64474f12bad
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-04 00:31:36 +0200

    Added course editing and adding and dropdown component to visualize available authors

--- src/components/courses/courseForm.js
+++ src/components/courses/courseForm.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const Input = require('../common/textInput');
+const Dropdown = require('../common/textDropdown');
 
 class CourseForm extends React.Component {
     constructor(props) {
@@ -10,10 +11,10 @@ class CourseForm extends React.Component {
         return(
             <form>
                 <Input name="title" label="Title" value={this.props.course.title} onChange={this.props.onChange} error={this.props.error.title}/>
-                <Input name="author" label="Author" value={this.props.course.author.name} onChange={this.props.onChange} error={this.props.error.author}/>
+                <Dropdown name="author" title="Authors" onChange={this.props.onChange} collection={this.props.authors} mapDropdownItem={(author) => `${author.firstName} ${author.lastName}`}/>
                 <Input name="category" label="Category" value={this.props.course.category} onChange={this.props.onChange} error={this.props.error.category}/>
                 <Input name="length" label="Length" value={this.props.course.length} onChange={this.props.onChange} error={this.props.error.length}/>
-                <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave}/>
+                <input value="Save" type="button" className="btn btn-default" onClick={this.props.onSave}/>
             </form>
         );
     }
--- src/components/courses/manageCoursePage.js
+++ src/components/courses/manageCoursePage.js
@@ -1,26 +1,31 @@
 const React = require('react');
 const CourseForm = require('./courseForm');
 const CourseApi = require('../../api/courseApi');
+const AuthorApi = require('../../api/authorApi');
 const toastr = require('toastr');
+const { Navigation } = require('react-router');
 
-class ManageCoursePage extends React.Component {
-    constructor(props) {
-        super(props);
+const ManageCoursePage = React.createClass({
+    getInitialState() {
+        const authors = AuthorApi.getAllAuthors();
 
-        this.state = {
+        return {
             course: {
                 title: '',
-                author: {
-                    id: '',
-                    name: ''
-                },
                 category: '',
-                length: ''
+                length: '',
+                author: {
+                    id: authors[0].id,
+                    name: `${authors[0].firstName} ${authors[0].lastName}`
+                },
             },
+            authors: AuthorApi.getAllAuthors(),
             error: {},
         }
-    }
-
+    },
+    mixins: [
+        Navigation
+    ],
     componentWillMount() {
         const courseId = this.props.params.id;
         if (courseId) {
@@ -28,17 +33,25 @@ class ManageCoursePage extends React.Component {
                 course: CourseApi.getCoursesById(courseId)
             });
         }
-    }
+    },
 
     setCourseState(event) {
         const field = event.target.name;
         const value = event.target.value;
 
-        this.state.course[field] = value;
+        if (field === 'author') {
+            this.state.course.author = {
+                id: this.state.authors.find(a => `${a.firstName} ${a.lastName}` === value).id,
+                name: value,
+            };
+        } else {
+            this.state.course[field] = value;
+        }
+
         this.setState({
             course: this.state.course
         });
-    }
+    },
 
     isFormValid() {
         const error = {};
@@ -58,25 +71,25 @@ class ManageCoursePage extends React.Component {
         });
 
         return isFormValid;
-    }
-
-    saveCourse(course) {
+    },
+    saveCourse() {
         if (!this.isFormValid()) {
             return;
         }
 
+        console.log(this.state.course, 'coures state');
+
         CourseApi.saveCourse(this.state.course);
         toastr.success('Course saved successfully!');
-    }
-
+        this.transitionTo('courses');
+    },
     render() {
-        console.log(this.state.course);
        return (<div>
             <h1>Manage Course</h1> 
-            <CourseForm course={this.state.course} error={this.state.error} onChange={this.setCourseState.bind(this)} onSave={this.saveCourse.bind(this)}/>
+            <CourseForm authors={this.state.authors} course={this.state.course} error={this.state.error} onChange={this.setCourseState} onSave={this.saveCourse}/>
         </div>
         );
-    }
-}
+    },
+});
 
 module.exports = ManageCoursePage;
